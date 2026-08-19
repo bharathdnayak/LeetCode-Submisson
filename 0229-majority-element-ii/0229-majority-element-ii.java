f20d06a5a00431317ca07ee2cd4f90e4d0cd0336
Bharath D Nayak <159288353+bharathdnayak@users.noreply.github.com>
2026-08-19 17:04:53 +0530

    Time: 2 ms (99.96%), Space: 52.6 MB (41.16%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,41 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        List<Integer> ans = new ArrayList<>();
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
 
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
         }
 
-        for (int num : map.keySet()) {
-            if (map.get(num) > nums.length / 3) {
-                ans.add(num);
-            }
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            if (num == candidate2) count2++;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (count1 > nums.length / 3) {
+            ans.add(candidate1);
+        }
+
+        if (count2 > nums.length / 3 && candidate2 != candidate1) {
+            ans.add(candidate2);
         }
 
         return ans;
