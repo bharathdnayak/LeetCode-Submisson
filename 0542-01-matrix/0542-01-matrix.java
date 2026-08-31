af06bd130045cbe13d7643bc34932b85be43faf5
Bharath D Nayak <159288353+bharathdnayak@users.noreply.github.com>
2026-08-31 22:57:49 +0530

    Time: 14 ms (71.26%), Space: 48.9 MB (75.41%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[][] dist = new int[m][n];
+        boolean[][] vis = new boolean[m][n];
+
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    q.offer(new int[]{i, j});
+                    vis[i][j] = true;
+                    dist[i][j] = 0;
+                }
+            }
+        }
+
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+            for (int k = 0; k < 4; k++) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && !vis[nr][nc]) {
+                    vis[nr][nc] = true;
+                    dist[nr][nc] = dist[r][c] + 1;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return dist;
+    }
+}
